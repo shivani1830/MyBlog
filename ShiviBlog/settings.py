0d24d380f0c0e5ed75dde8ef63ce1bf1c89f8138
shivani1830 <shivanianbu2002@gmail.com>
2024-01-24 15:05:11 +0530

    NavBar

--- ShiviBlog/settings.py
+++ ShiviBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hmjyzh-sn%0lhflurbg@#i3u%r*deeof98&oefvuc782a%dk7r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app', '127.0.0.1']
 
 
 # Application definition
